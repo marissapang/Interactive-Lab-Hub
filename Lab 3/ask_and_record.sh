37fde90fb8b0d2f0b04070bffc891e7fdecd5532
marissapang <marissapang@live.com>
2021-10-04 22:43:22 -0400

    lab 3 part 1

---
+++ Lab 3/ask_and_record.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+say() { local IFS=+;/usr/bin/mplayer -ao alsa -really-quiet -noconsolecontrols "http://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q=$*&tl=en"; }
+#say $*
+say "Hello, coud you give me your phone number please? "
+
+#arecord -f cd -r 16000 -d 5 -t wav recorded.wav && sox recorded.wav recorded_mono.wav remix 1,2
+
+arecord -D hw:2,0 -f cd -c1 -r 48000 -d 5 -t wav recorded_mono.wav
+python3 test_words.py recorded_mono.wav
